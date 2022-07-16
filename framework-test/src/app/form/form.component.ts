871c0023c0ff89e3ad54a563485e9d04f4a27f90
tsouza-cs <tsouza@creditshop.com>
2022-07-16 16:26:42 -0300

    first structure

---
+++ framework-test/src/app/form/form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+  username: FormControl = new FormControl('');
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
